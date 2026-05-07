32638231720671f013441e532c988469a2a5de13
Shashi <pagadal.shashi@gmail.com>
2026-05-07 14:52:17 +0530

    fixed mongodb connection

--- my-app/lib/dbConnect.js
+++ my-app/lib/dbConnect.js
@@ -1,14 +1,50 @@
 import mongoose from "mongoose";
-const MONGODB_URI = "mongodb://localhost:27017/" ; 
 
-const dbConnect = async () => {
-  try {
-    await mongoose.connect(MONGODB_URI);
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    process.exit(1);
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+
+  throw new Error("Please define MONGODB_URI");
+
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+
+  cached = global.mongoose = {
+    conn: null,
+    promise: null,
+  };
+
+}
+
+async function dbConnect() {
+
+  if (cached.conn) {
+
+    return cached.conn;
+
   }
-};
+
+  if (!cached.promise) {
+
+    cached.promise = mongoose.connect(MONGODB_URI, {
+
+      bufferCommands: false,
+
+    }).then((mongoose) => {
+
+      return mongoose;
+
+    });
+
+  }
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+
+}
 
 export default dbConnect;
